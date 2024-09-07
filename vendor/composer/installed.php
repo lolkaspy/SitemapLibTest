4086336c5dbe2442b8081e0bcae914e07d482f23
belyash1337 <opdata27@gmail.com>
2024-09-08 02:14:28 +0700

    change composer.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '637ee57f2ebe57f631a3da9cb088e0b378e2a788',
         'name' => 'belyashdima-test-tasks/sitemap-lib-test',
         'dev' => true,
     ),
     'versions' => array(
         'belyashdima-test-tasks/sitemap-lib-test' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '637ee57f2ebe57f631a3da9cb088e0b378e2a788',
             'dev_requirement' => false,
         ),
     ),
